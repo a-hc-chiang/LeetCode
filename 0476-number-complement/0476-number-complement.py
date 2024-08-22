0aa20abe7b095dce663ae4f159f8562c16f4d3ae
A. Chiang <143686734+a-hc-chiang@users.noreply.github.com>
2024-08-22 00:27:42 -0700

    Time: 30 ms (84.59%), Space: 16.6 MB (14.64%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -4,11 +4,10 @@ class Solution:
         binaryRep = ""
         while num > 0: #convert to binary with modulo 
             if num % 2 == 1: 
-                binaryRep += "1"
+                binaryRep = "1" + binaryRep 
             else: 
-                binaryRep += "0"
+                binaryRep = "0" + binaryRep
             num = floor(num/2)
-        binaryRep = binaryRep[::-1] #reverse string 
         exp = 0 
         for i in binaryRep[::-1]: #convert to decimal 
             if i == "0": # switch condition for complement 
