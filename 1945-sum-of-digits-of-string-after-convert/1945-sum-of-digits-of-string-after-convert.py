f29fbbfd66b16af803808c9078edab6301807ebf
A. Chiang <143686734+a-hc-chiang@users.noreply.github.com>
2024-09-02 22:12:58 -0700

    Time: 37 ms (66.35%), Space: 16.5 MB (73.84%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        res = 0
+        resStr = ""
+        for c in s: 
+            resStr = str((ord(c) - 96)) + resStr #taking each character's ascii values to integers 
+        for i in range(k):
+            res = 0 #reseting the result for each iteration of k 
+            for c in resStr: 
+                res += ord(c) - 48  
+            resStr = str(res)
+        return res
